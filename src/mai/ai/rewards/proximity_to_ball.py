200e4a6cae61c2616c614486347996ad17da97af
Articha <tima-1324@mail.ru>
2024-12-18 22:45:22 +0300

    Trying to normalize rewards

--- src/mai/ai/rewards/proximity_to_ball.py
+++ src/mai/ai/rewards/proximity_to_ball.py
@@ -4,13 +4,14 @@ from mai.capnp.data_classes import Vector
 from mai.settings import Settings
 
 class NNReward(NNRewardBase):
-    __slots__ = ('latest_distance',)
-    _multiplier = 40
-    _difference_decay = 0.01
+    __slots__ = ('multiplier', 'latest_distance', 'counter')
+    _divider = 0.7
 
     def __init__(self) -> None:
         super().__init__()
         self.latest_distance = None
+        self.multiplier = self._divider
+        self.counter = 0
 
     def _calculate(self, state, context) -> float:
         car_pos = Vector.from_mai(state.car.position)
@@ -20,7 +21,14 @@ class NNReward(NNRewardBase):
             self.latest_distance = distance
             return 0
 
+        if self.counter == 10:
+            self.multiplier = Settings.get_current_eps() / self._divider
+            self.counter = 0
+        else:
+            self.counter += 1
+
         difference = self.latest_distance-distance
         self.latest_distance = distance
 
-        return difference
+        result = difference*self.multiplier
+        return result
--- src/mai/plotter.py
+++ src/mai/plotter.py
@@ -8,10 +8,8 @@ import matplotlib.pyplot as plt
 
 class ChildProcessPlotter:
     __slots__ = (
-        'pipe',
-        'fig', 'ax',
-        'x', 'ys',
-
+        'pipe', 'fig', 'ax',
+        'x', 'ys', 'xlim', 'ylim',
         'plot_names',
     )
 
@@ -35,6 +33,7 @@ class ChildProcessPlotter:
                     y.pop(0)
                     y.append(command[i])
                     self.ax.plot(self.x, y, label=self.plot_names[i])
+                    self.ax.set_ylim(*self.ylim)
                 self.ax.legend()
         self.fig.canvas.draw()
         return True
@@ -48,12 +47,13 @@ class ChildProcessPlotter:
     ):
         self.pipe = pipe
         self.plot_names = plot_names
+        self.xlim = xlim
+        self.ylim = ylim
 
         self.x = list(range(xlim[1]))
         self.ys = [[None] * xlim[1] for _ in range(len(plot_names))]
 
         self.fig, self.ax = plt.subplots()
-        self.ax.set_xlim(*xlim)
         self.ax.set_ylim(*ylim)
         timer = self.fig.canvas.new_timer(interval=0)
         timer.add_callback(self.call_back)
