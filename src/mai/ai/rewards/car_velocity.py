23210de8e7eeb53cebf5b3cecc955a92e5c8555d
Articha <tima-1324@mail.ru>
2024-12-19 19:47:23 +0300

    Finally fixed car velocity values!

--- src/mai/ai/rewards/car_velocity.py
+++ src/mai/ai/rewards/car_velocity.py
@@ -4,16 +4,14 @@ from mai.capnp.data_classes import Vector
 from mai.settings import Settings
 
 class NNReward(NNRewardBase):
-    __slots__ = ('addition', 'multiplier')
+    __slots__ = ('multiplier',)
 
     def __init__(self) -> None:
         super().__init__()
-        self.addition = Settings.max_speed_magnitude / 10
-        self.multiplier = 0.2
+        self.multiplier = 1/0.561
 
     def _calculate(self, state, context) -> float:
         car_velocity = Vector.from_mai(state.car.velocity)
         magnitude = car_velocity.magnitude() - context.magnitude_offsets['car']['v']
-        magnitude -= self.addition
         magnitude *= self.multiplier
-        return magnitude if magnitude > 0 else 0
+        return magnitude if magnitude < 1 else 1
