5992ca65d73cec44f3e807460cd0fa6ef0e64167
Articha <tima-1324@mail.ru>
2025-01-03 04:41:09 +0300

    Slight renaming of module

--- src/mai/ai/networks/reachBallFloor.py
+++ src/mai/ai/networks/reachBallFloor.py
@@ -15,27 +15,13 @@ class NNModule(NNModuleBase):
         'state.car.rotation.pitch',
         'state.car.rotation.roll',
         'state.car.rotation.yaw',
-        'state.car.angularVelocity.x',
-        'state.car.angularVelocity.y',
-        'state.car.angularVelocity.z',
         'state.ball.position.x',
         'state.ball.position.y',
         'state.ball.position.z',
-        'state.ball.velocity.x',
-        'state.ball.velocity.y',
-        'state.ball.velocity.z',
     )
     output_types = (
         'controls.throttle',
         'controls.steer',
-        'controls.pitch',
-        'controls.yaw',
-        'controls.roll',
-        'controls.boost',
-        'controls.jump',
-        'controls.handbrake',
-        'controls.dodgeVertical',
-        'controls.dodgeHorizontal',
     )
 
     @classmethod
