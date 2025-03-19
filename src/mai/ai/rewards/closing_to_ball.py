cee5526f63c4a6403378ffc99d3a88a6a20e7a52
Articha <tima-1324@mail.ru>
2025-03-19 22:44:22 +0300

    Still trying to fix this reward

--- src/mai/ai/rewards/closing_to_ball.py
+++ src/mai/ai/rewards/closing_to_ball.py
@@ -7,22 +7,26 @@ from mai.capnp.data_classes import Vector
 class NNReward(NNRewardBase):
     __slots__ = (
         'multiplier',
-        'latest_distance', 'latest_answer', 
+        'latest_distance', 'latest_answer', 'iteration',
         'result',
     )
     _multiplier: float = 3.
     _update_seconds: float = 0.2
+    iteration: int
 
     def __init__(self) -> None:
         super().__init__()
         self.latest_distance = None
         self.multiplier = self._multiplier
+        self.iteration = 0
 
     def reset(self) -> None:
         super().reset()
         self.latest_distance = None
+        self.iteration = 0
 
     def _calculate(self, state, context) -> float:
+        self.iteration += 1
         car = Vector.from_mai(state.car.position)
         ray = (Vector.from_mai(state.ball.position)-car)
         distance = ray.magnitude()
@@ -37,9 +41,13 @@ class NNReward(NNRewardBase):
             # Positive -> we are closer to the ball
             difference = self.latest_distance - distance
             if difference < 0:
-                self.result = difference * self.multiplier
+                result = difference * self.multiplier
             else:
-                self.result = difference**0.7 * self.multiplier
+                result = difference**0.7 * self.multiplier
+            if abs(result-self.result) > 0.1:
+                self.result += (result - self.result)*0.3
+            else:
+                self.result = result
             self.latest_distance = distance
             self.latest_answer = current
         return self.result
