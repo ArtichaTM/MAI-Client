37369c69703ed853df8b22429a93efff1db4465b
Articha <tima-1324@mail.ru>
2025-04-06 17:43:35 +0300

    Trying to implement reachBall func

--- src/mai/ai/networks/reachBallFloor.py
+++ src/mai/ai/networks/reachBallFloor.py
@@ -51,37 +51,42 @@ class Module(ModuleBase):
         assert isinstance(requires_grad, bool)
         state = tensor_dict.origin
         assert state is not None
-        ball_vector = np.array([
+        ball_pos = np.array([
             state.ball.position.x,
             state.ball.position.y,
             state.ball.position.z
         ])
-        car_vector = np.array([
+        car_pos = np.array([
             state.car.position.x,
             state.car.position.y,
             state.car.position.z
         ])
-        car_rotation = (
-            state.car.rotation.pitch,
-            state.car.rotation.yaw,
-            state.car.rotation.roll
-        )
-        direction_vector = ball_vector - car_vector
+        car_velocity = np.array([
+            state.car.velocity.x,
+            state.car.velocity.y,
+            state.car.velocity.z,
+        ])
+        car_yaw = state.car.rotation.yaw
 
-        # Calculate the car's forward vector
-        forward_vector = np.array([np.cos(np.pi * car_rotation[1]) * np.cos(np.pi * car_rotation[0]),
-                                np.sin(np.pi * car_rotation[0]),
-                                np.sin(np.pi * car_rotation[1]) * np.cos(np.pi * car_rotation[0])])
+        throttle, steer = 1, 0
+        direction_vector = ball_pos - car_pos
+        if abs(car_velocity.max()) < 0.01:
+            steer = 1
+        else:
+            direction_vector = direction_vector/np.linalg.norm(direction_vector)
+            car_velocity = car_velocity/np.linalg.norm(car_velocity)
 
-        # Calculate the car's right vector
-        right_vector = np.cross(forward_vector, np.array([0, 1, 0]))
-        right_vector = right_vector / np.max(np.abs(right_vector))
-        throttle = np.dot(direction_vector, forward_vector)
-        steer = np.dot(direction_vector, right_vector)
-        yaw = np.arctan2(direction_vector[1], direction_vector[0]) / np.pi
-        throttle, steer, yaw = map(lambda x: round(x, 4), (
-            throttle, steer, yaw
-        ))
+            dot = np.dot(direction_vector, car_velocity)
+            angle = np.arccos(dot)
+            cross = np.cross(direction_vector, car_velocity)
+            sign = np.sign(cross[2])
+            if angle > 2:
+                steer = 1
+            elif sign > 0:
+                steer = -1
+            else:
+                steer = 1
+            print(angle, sign)
 
         throttle = torch.tensor(throttle, requires_grad=requires_grad)
         if 'controls.throttle' in tensor_dict:
@@ -93,11 +98,11 @@ class Module(ModuleBase):
             tensor_dict['controls.steer'].append(steer)
         else:
             tensor_dict['controls.steer'] = [steer]
-        yaw = torch.tensor(yaw, requires_grad=requires_grad)
-        if 'controls.yaw' in tensor_dict:
-            tensor_dict['controls.yaw'].append(yaw)
-        else:
-            tensor_dict['controls.yaw'] = [yaw]
+        # yaw = torch.tensor(yaw, requires_grad=requires_grad)
+        # if 'controls.yaw' in tensor_dict:
+        #     tensor_dict['controls.yaw'].append(yaw)
+        # else:
+        #     tensor_dict['controls.yaw'] = [yaw]
 
     def requires(self) -> set[str]:
         return set()
